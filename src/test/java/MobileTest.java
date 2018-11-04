1ecdbe7d3669aa406ca33014fed273750ee376ba
asciishell <asciishell@yandex.ru>
2018-11-04 12:27:13 +0300

    Add select

--- src/test/java/MobileTest.java
+++ src/test/java/MobileTest.java
@@ -1,9 +1,81 @@
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MobileTest extends BaseRunner {
 
+    public class SelectInput {
+        private WebElement element;
+
+        SelectInput(String name) {
+            new WebDriverWait(driver, 5).until(d -> driver.findElements(By.xpath("//span[contains(text(),'" + name + "')]/ancestor::div[@class='ui-dropdown-field']")).size() != 0);
+            element = driver.findElement(By.xpath("//span[contains(text(),'" + name + "')]/ancestor::div[@class='ui-dropdown-field']"));
+        }
+
+        private List<WebElement> getAllItems() {
+            return element.findElements(By.xpath(".//div[@class='ui-dropdown-field-list__item-view ui-select__option_with-subtext_right-side']"));
+        }
+
+        private boolean isMaximized() {
+            return getAllItems().get(0).isDisplayed();
+        }
+
+        private void maximize() {
+            if (!isMaximized()) {
+                element.click();
+                new WebDriverWait(driver, 5).until(d -> isMaximized());
+            }
+        }
+
+        private void minimize() {
+            if (isMaximized()) {
+                element.click();
+                new WebDriverWait(driver, 5).until(d -> !isMaximized());
+            }
+        }
+
+        List<String> getAllItemsText() {
+            List<String> result = new ArrayList<>();
+            List<WebElement> els = getAllItems();
+            maximize();
+            for (WebElement el : els) {
+                result.add(el.getText());
+            }
+            minimize();
+            return result;
+        }
+
+        String getCurrentItemText() {
+            return element.findElement(By.xpath("//div[@class='ui-select__title-flex']")).getText();
+        }
+
+        void setItem(int index) {
+            maximize();
+            getAllItems().get(index).click();
+        }
+
+        void setItem(String text) {
+            maximize();
+            new WebDriverWait(driver, 5).until(d ->
+                    {
+                        List<WebElement> els = getAllItems();
+                        for (WebElement el : els) {
+                            if (el.getText().contains(text)) {
+                                el.click();
+                                return true;
+                            }
+                        }
+                        return false;
+                    }
+            );
+        }
+    }
 
     public class CheckBox {
         private String name;
